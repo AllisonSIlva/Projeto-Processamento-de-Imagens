b8b1f87e89180be3c2041f3c66c6af2b4c3e43cd
AllisonSIlva <74565574+AllisonSIlva@users.noreply.github.com>
2022-05-02 00:59:51 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,7 +1,21 @@
 import java.io.IOException;
 
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+
 public class main {
 	public static void main(String[] args) throws IOException{
+		
+		try {
+		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+		        if ("Nimbus".equals(info.getName())) {
+		            UIManager.setLookAndFeel(info.getClassName());
+		            break;
+		        }
+		    }
+		} catch (Exception e) {
+		    // If Nimbus is not available, you can set the GUI to another look and feel.
+		}
 		Aula content = new Aula();
 		content.conteudo();
 		
